6c8da514b33d9dde27a2fb0e337ec172431d572d
Tzen <seanie626@gmail.com>
2024-04-23 19:38:03 -0700

    lab 4 summary

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,26 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Pseudocode - Watercolor Painting and RPS
+// Author: Sean Yabut
+// Date: 4/23/24
 
-// Constants
+// Watercolor Painting:
 
-// Functions
+ // 1. Find an area to work
+ // 2. Gather materials (paper, paint, paintbrushes, water, cup, paper towel or rag )
+ // 3. Fill a cup with water halfway
+ // 4. Dip your cup into the water
+ // 5. Remove excess water from your paintbrush
+ // 6. Swirl your paintbrush in the desired color in your paint palette
+ // 7. Put the paintbrush to your paper, paint to the heart’s content 
+ // 8. To switch colors, swirl the paintbrush in water
+ // 9. Repeat steps 5-7 until you are happy with your art
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+ // Rock Paper Scissors
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+ // 1. Ask the user how many rounds they want to play for
+ // 2. Prompt user to enter one of three inputs (R./P./S. respectively)
+ // 3. Randomly generate an answer (Rock, Paper, or Scissors)
+ // 4. Win/calculative functions:
+ // 5. Score assignment: check who gets the point out of the pre-established rounds/variable
+    // 5a. Win conditions: check what beats what
+    // 5b. I.e “If player enters paper against scissors, then player gets point”
+ // 8. Loop steps 2-4 until the round counter reaches the max it was set to
