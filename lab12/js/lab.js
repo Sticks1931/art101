f08604c4a9ddc365e3927d5ffaa233e4a72ee753
Tzen <seanie626@gmail.com>
2024-05-23 18:24:29 -0700

    yeah

---
+++ lab12/js/lab.js
@@ -0,0 +1,35 @@
+// Lab 12: Conditionals
+// Author: Sean Yabut
+// Date: 5/23/24
+
+// Take name input then sort based on string length
+function sortingHat(str) {
+     length = str.length;
+     mod = length % 4;
+
+    if (mod == 0) {
+        return "Gryffindor";
+    }
+    else if (mod == 1) {
+        return "Ravenclaw";
+    }
+    else if (mod == 2) {
+        return "Syltherin";
+    }
+    else if (mod == 3) {
+        return "Hufflepuff";
+    }
+ }
+
+// Event Listener
+$("#button").click(function() {
+    
+    // Sets str to user input
+    const name = $("input").val();
+
+    // Sets str to sortingHat's output
+    const house = sortingHat(name);
+
+    // Prints the user's assigned house
+    $("#output").html('<div class="text"><p>' + "Your assigned house is: " + house + '</p></div>');
+})
